874db35c1f9a976d8cfefe9536fc425547ad5db8
dev-14 <devansh.bagaria@somaiya.edu>
2021-11-02 13:28:37 +0530

    addToCart system working for each user

--- controllers/orders.go
+++ controllers/orders.go
@@ -10,15 +10,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// CreateBook godoc
-// @Summary CreateBook endpoint is used by the supervisor role user to create a new book.
-// @Description CreateBook endpoint is used by the supervisor role user to create a new book
-// @Router /api/v1/auth/books/create [post]
+// AddToCart godoc
+// @Summary AddToCart endpoint is used to add the book to the cart.
+// @Description AddToCart endpoint is used to add the book to the cart.
+// @Router /api/v1/auth/cart/add [post]
 // @Tags book
 // @Accept json
 // @Produce json
-// @Param name formData string true "name of the book"
-// @Param category_id formData string true "category_id of the book"
+// @Param title formData string true "title of the book"
 func AddToCart(c *gin.Context) {
 
 	//var existingBook models.Book
@@ -55,12 +54,12 @@ func AddToCart(c *gin.Context) {
 	// }
 
 	//Check if the product already exists.
-	err := models.DB.Where("title = ?").First(&Book).Error
-	if err == nil {
+	err := models.DB.Where("title = ?", title).First(&Book).Error
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "book does not exist."})
 		return
 	}
-	fmt.Println(Book.Price)
+	fmt.Println(Book)
 	cart := models.Cart{
 		// BookName: Book.Title,
 		// Price:    Book.Price,
@@ -78,4 +77,31 @@ func AddToCart(c *gin.Context) {
 		"name":    title,
 	})
 
+}
+
+// ViewCart godoc
+// @Summary ViewCart endpoint is used to list all book.
+// @Description API Endpoint to view the cart items.
+// @Router /api/v1/auth/cart/view [get]
+// @Tags book
+// @Accept json
+// @Produce json
+func ViewCart(c *gin.Context) {
+
+	// allProduct := []models.Product{}
+	claims := jwt.ExtractClaims(c)
+	user_email, _ := claims["email"]
+	var User models.User
+	//var Book []models.Book
+	//var existingBook []ReturnedBook
+	//var Cart models.Cart
+
+	if err := models.DB.Where("email = ?", user_email).First(&User).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+	//if err = models.DB.Where("")
+	//models.DB.Model(Cart).Find(&existingBook)
+	//c.JSON(http.StatusOK, existingBook)
+	return
 }
--- models/cart.go
+++ models/cart.go
@@ -3,9 +3,9 @@ package models
 type Cart struct {
 	CartID int
 	UserID int
-	User User `gorm:foreignKey:"UserID" json:"-"`
+	User   User `gorm:foreignKey:"UserID"`
 	BookID int
-	Book Book `gorm:foreignKey:"BookID" json:"-"`
+	Book   Book `gorm:foreignKey:"BookID"`
 	// BookName string
 	// Price int
 }
